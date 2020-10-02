73ec6f9953bbf04d178193775a9fa37f24c98235
evanjh <71201536+evanjh@users.noreply.github.com>
2020-10-02 18:15:48 +0800

    Add files via upload

---
+++ example.config.py
@@ -0,0 +1,20 @@
+
+# 支付超时时间(秒)
+PAY_TIMEOUT = 300
+# BOT API
+TOKEN = '1278323386:AAEGAvLEZt93LD9yMxmd6_LYAH4rPXZj21A'
+# ADMIN ID
+ADMIN_ID = [1181627796]
+
+# 管理员命令
+ADMIN_COMMAND_START = 'iadmin'
+ADMIN_COMMAND_QUIT = 'icancel'
+
+# 支持的支付方式
+PAYMENT_METHOD = {
+    'epay': '支付宝/微信/QQ',
+    'alifacepay': '支付宝当面付',
+}
+
+# 当前版本
+VERSION = '1.3.1'
